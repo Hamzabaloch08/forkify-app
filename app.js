0dd339632d752f265c056dd1c87d6592df503e60
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-03-03 19:39:02 +0500

    checking repo

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 let searchValue = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
-const searchRecipe = (event) => {
-  event.preventDefault();
+// const searchRecipe = (event) => {
+//   event.preventDefault();
+//   searchValue = searchInput.value;
+//   const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchValue}`;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data.data, "data");
+//       const recipe = data.data.recipes;
+//       console.log(recipe, "recipe");
+//       recipe.forEach((element) => {
+//         console.log(element, "title");
+//       });
+//     });
+// };
+
+let searchRecipes = async () => {
   searchValue = searchInput.value;
   const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchValue}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.data, "data");
-      const recipe = data.data.recipes;
-      console.log(recipe, "recipe");
-      recipe.forEach((element) => {
-        console.log(element, "title");
-      });
-    });
-};
 
-searchBtn.addEventListener("click", searchRecipe);
+  try {
+    const recipeData = await fetch(url);
+    const data = await recipeData.json();
+    const recipe = data.data.recipes;
+    console.log(recipe);
+    recipe.forEach((element) => {
+      console.log(element);
+    });
+  } catch (error) {
+    console.log("error: ", error);
+  }
+};
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  searchRecipes();
+});
