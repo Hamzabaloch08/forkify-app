d3b9884993cc31321e7804c5cdf6a6a304c13c9b
Hamzabaloch08 <hb.developer08@gmail.com>
2025-03-03 19:53:11 +0500

    checking repo

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 let searchValue = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
-// const searchRecipe = (event) => {
-//   event.preventDefault();
-//   searchValue = searchInput.value;
-//   const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchValue}`;
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       console.log(data.data, "data");
-//       const recipe = data.data.recipes;
-//       console.log(recipe, "recipe");
-//       recipe.forEach((element) => {
-//         console.log(element, "title");
-//       });
-//     });
-// };
 
 let searchRecipes = async () => {
   searchValue = searchInput.value;
