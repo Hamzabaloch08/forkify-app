bc61a69e818ed6e1b2772483bf00d6b92037c23b
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-03-03 17:05:49 +0500

    function fixed

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 let searchValue = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
 
-const searchRecipe = () => {
+const searchRecipe = (event) => {
+  event.preventDefault();
   searchValue = searchInput.value;
   const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchValue}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data, "data");
+      console.log(data.data, "data");
+      const recipe = data.data.recipes;
+      console.log(recipe, "recipe");
+      recipe.forEach((element) => {
+        console.log(element, "title");
+      });
     });
 };
 
