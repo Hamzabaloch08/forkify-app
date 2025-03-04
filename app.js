b3789204cb08b3cc0575f6aa21ef236e2cdbe871
Muhammad Uzair <muhammaduzairbaloch52@gmail.com>
2025-03-04 16:16:08 +0500

    Recipe List Added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let searchValue = document.getElementById("searchInput");
 let searchBtn = document.getElementById("searchBtn");
+let recipeList = document.querySelector(".card-list");
 
 let searchRecipes = async () => {
   searchValue = searchInput.value;
@@ -10,6 +11,16 @@ let searchRecipes = async () => {
     const data = await recipeData.json();
     const recipe = data.data.recipes;
     console.log(recipe);
+    recipeList.innerHTML = "";
+    recipe.forEach((element) => {
+      recipeList.innerHTML += `
+      <div style="padding: 5px 0 0 5px" class="card">
+      <img style="width: 100px; height:100px; border-radius: 50px";  src="${element.image_url}" alt="recipe image" />
+      <p>${element.title}</p>
+      <p>${element.publisher}</p>
+      </div>
+      `;
+    });
   } catch (error) {
     console.log("error: ", error);
   }
