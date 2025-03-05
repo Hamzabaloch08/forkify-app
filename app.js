03e6626a90d7ba17391b4162d5f1846900a48a42
Hamzabaloch08 <hb.developer08@gmail.com>
2025-03-05 16:14:47 +0500

    Fix: Fetch recipe details dynamically based on clicked card ID

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ let recipeList = document.querySelector(".card-list");
 let searchRecipes = async () => {
   searchValue = searchInput.value;
   const url = `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchValue}`;
-
   try {
     const recipeData = await fetch(url);
     const data = await recipeData.json();
     const recipe = data.data.recipes;
-    console.log(recipe);
-    recipeList.innerHTML = "";
     recipe.forEach((element) => {
       recipeList.innerHTML += `
-      <div style="padding: 5px 0 0 5px" class="card">
+    
+      <div id='${element.id}' style="padding: 5px 0 0 5px" class="card">
+
       <img style="width: 100px; height:100px; border-radius: 50px";  src="${element.image_url}" alt="recipe image" />
       <p>${element.title}</p>
       <p>${element.publisher}</p>
@@ -26,7 +25,19 @@ let searchRecipes = async () => {
   }
 };
 
+let searchSingleRecipies = async () => {
+  let card = document.querySelector(".card");
+  let id = card.id;
+  console.log(id);
+  const response = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+  );
+  const JSONResponse = await response.json();
+  console.log(JSONResponse.data.recipe);
+};
+
 searchBtn.addEventListener("click", (event) => {
   event.preventDefault();
   searchRecipes();
 });
+ 
